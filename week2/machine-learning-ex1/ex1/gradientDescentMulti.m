12fba3db70c8d29ffd11e336312f5ec29fa55e0c
Leiver Andres Campeón <leiverandres04p@hotmail.com>
2017-03-02 19:31:28 -0500

    Add gradient descent and cost funtion for multivar, also add normal equation

--- week2/machine-learning-ex1/ex1/gradientDescentMulti.m
+++ week2/machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -11,25 +11,16 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
-
+    H = X * theta;
+    theta = theta - (alpha / m) * X' * (H - y);
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
